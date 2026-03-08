c0fbd946a235b46a5553f5c077e93ccfad6a5ec4
kengarshridhar <kengarshridhar007@outlook.com>
2026-03-09 00:35:03 +0530

    Update user schema

--- models/mongo/User.js
+++ models/mongo/User.js
@@ -1,11 +1,24 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  phone: String,
-  username: String,
-  otp: String,
-  oauthProvider: String,
+  email: { type: String, trim: true, lowercase: true },
+  phone: { type: String},
+  username: {type: String, required: true },
+  otp: { type: String },
+  oauthProvider: { type: String },
+  role: { 
+    type: String, 
+    enum: [
+      "super_admin",
+      "admin",
+      "sales",
+      "editor",
+      "seo_editor",
+      "analytics",
+      "customer"
+    ],
+    default: "customer"
+  }
 });
 
 export default mongoose.model("User", userSchema);
