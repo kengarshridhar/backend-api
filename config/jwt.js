d374845e58de9cb5d19fe7c99bdd7ff9656c227e
kengarshridhar <kengarshridhar007@outlook.com>
2026-03-11 01:26:08 +0530

    [add] refresh token

--- config/jwt.js
+++ config/jwt.js
@@ -7,6 +7,17 @@ export const generateToken = (user) => {
       role: user.role
     },
     process.env.JWT_SECRET,
+    { expiresIn: '2h'}
+  );
+};
+
+export const refreshToken = (user) => {
+  return jwt.sign(
+    {
+      id: user._id,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
     { expiresIn: '1d'}
   );
 };
