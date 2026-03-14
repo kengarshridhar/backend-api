e63fdbdf49bb27f3cbd76d94fbda13fa6bff8b44
kengarshridhar <kengarshridhar007@outlook.com>
2026-03-15 00:40:28 +0530

    [update] all files update in app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import helmet from "helmet";
+import corsMiddleware from './config/cors.js'
 
 import sessionMiddleware from './config/session.js';
+import { apiLimiter } from './middlewares/rateLimiter.js';
+import { requestLogger } from './middlewares/auth/requestLogger.js';
 
 import authRoutes from './routes/authRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
@@ -11,11 +15,14 @@ import protectRoutes from './routes/protectRoutes.js'
 // ecom routes
 import categoryRoute from './routes/ecom/categoryRoutes.js'
 import productRoute from './routes/ecom/productRoutes.js'
-import { apiLimiter } from './middlewares/rateLimiter.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
-app.use(express.json());
 app.use(sessionMiddleware);
+app.use(helmet());
+app.use(corsMiddleware);
+app.use(express.json());
+app.use(requestLogger);
 
 // Use routes
 app.use("/api", apiLimiter)
@@ -27,5 +34,7 @@ app.use("/users", userRoutes);
 // ecom route
 app.use("/api/product", productRoute);
 app.use("/api/category", categoryRoute);
+
+app.use(errorHandler);
 
 export default app;
